1cad45f615d86042d4814ed49063e22314e40286
Steve Striker <stever185@gmail.com>
2021-12-22 18:44:05 +0100

    second

---
+++ src/Components/Avatar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { AvatarData } from "../Utils/AvatarData";
+import "../App.css";
+
+const Avatar = () => {
+  return (
+    <div className='avatar-bar'>
+      Avatars
+      {AvatarData.map((avatar, index) => {
+        return <img src={avatar.image} alt='lost' className='guild' />;
+      })}
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ src/Utils/AvatarData.js
@@ -0,0 +1,29 @@
+import Aave from "../Images/Aave.png";
+import Agave from "../Images/Agave.png";
+import Curve from "../Images/Curve.png";
+import Gitcoin from "../Images/Gitcoin.png";
+import Pharo from "../Images/Pharo.png";
+import Skull from "../Images/Skull.png";
+import Inch from "../Images/1inch.png";
+import Hive from "../Images/1Hive.png";
+import Compass from "../Images/Compass.png";
+import Discord from "../Images/Discord.png";
+import Sushiswap from "../Images/Sushiswap.png";
+import Tec from "../Images/TEC.png";
+import Uniswap from "../Images/Uniswap.png";
+
+export const AvatarData = [
+  { image: Aave },
+  { image: Agave },
+  { image: Curve },
+  { image: Pharo },
+  { image: Skull },
+  { image: Inch },
+  { image: Gitcoin },
+  { image: Hive },
+  { image: Compass },
+  { image: Discord },
+  { image: Sushiswap },
+  { image: Tec },
+  { image: Uniswap },
+];
